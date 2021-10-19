f178d9fb47dff575c311bc771bb96c6facdc72ca
Tom Ridge <tom.j.ridge@googlemail.com>
2021-10-19 16:02:59 +0100

    +

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,8 @@
 (** Main binary *)
 
 open Kv_hash
+open Kv_hash.Private.Util
+open Partition
 
 (** Use a sequence so we can start printing immediately *)
 let list_values ~fn = 
@@ -9,8 +11,24 @@ let list_values ~fn =
       Printf.printf "%S %d\n%!" s off)
 
 
+let show_partition ~fn = 
+  let p = Partition_ii.read_fn ~fn in
+  let xs = Partition_ii.to_list p in
+  xs |> List.iter (fun (k,v) -> 
+      Printf.printf "%d %d\n%!" k v)
+
+let show_buckets ~fn =
+  let bs = Bucket_store.Bucket_store0.open_ ~fn in
+  1 |> iter_k (fun ~k:kont i -> 
+      let b = Bucket_store0.read_bucket bs i in
+      Printf.printf "Bucket %d: \n%!" i;
+      Bucket0.show b.raw_bucket;
+      kont (i+1))
+
 let _ = 
   match Sys.argv |> Array.to_list |> List.tl with
   | ["list_values";fn] -> list_values ~fn
+  | ["show_partition";fn] -> show_partition ~fn
+  | ["show_buckets";fn] -> show_buckets ~fn
   | _ -> failwith ""
   
